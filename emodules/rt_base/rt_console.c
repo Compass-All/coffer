bdf04e01f6bd1686c23a808faa1f160de8d12042
Tert-butyllithium <lihaonan1105@gmail.com>
2021-10-26 19:12:54 +0800

    update for qemu

--- emodules/rt_base/rt_console.c
+++ emodules/rt_base/rt_console.c
@@ -29,107 +29,107 @@ static void putstring(const char* s)
     } while (0)
 
 // FIXME wip to replace below function
-static void rt_print(const char* fmt, va_list vl)
-{
-    char buf[CONSOLE_BUF_SIZE];
-    fmt_flag_t fmt_flags = 0;
-    int acnt, fmt_width;
-    int i = 0;
-    uint64_t num;
-    char* s;
+// static void rt_print(const char* fmt, va_list vl)
+// {
+//     char buf[CONSOLE_BUF_SIZE];
+//     fmt_flag_t fmt_flags = 0;
+//     int acnt, fmt_width;
+//     int i = 0;
+//     uint64_t num;
+//     char* s;
 
-    for (; *fmt; ++fmt) {
-        if (*fmt == '%') {
-            ++fmt;
-            fmt_width = fmt_flags = 0;
-            if (*fmt == '\0') {
-                break;
-            }
-            if (*fmt == '%') {
-                goto out;
-            }
+//     for (; *fmt; ++fmt) {
+//         if (*fmt == '%') {
+//             ++fmt;
+//             fmt_width = fmt_flags = 0;
+//             if (*fmt == '\0') {
+//                 break;
+//             }
+//             if (*fmt == '%') {
+//                 goto out;
+//             }
 
-            // Parse flags
-            if (*fmt == '-') {
-                ++fmt;
-                fmt_flags |= FLAG_PAD_RIGHT;
-            }
-            while (*fmt == '0') {
-                ++fmt;
-                fmt_flags |= FLAG_PAD_ZERO;
-            }
+//             // Parse flags
+//             if (*fmt == '-') {
+//                 ++fmt;
+//                 fmt_flags |= FLAG_PAD_RIGHT;
+//             }
+//             while (*fmt == '0') {
+//                 ++fmt;
+//                 fmt_flags |= FLAG_PAD_ZERO;
+//             }
 
-            // Parse width
-            for (; isdigit(*fmt); ++fmt) {
-                fmt_width *= 10;
-                fmt_width += *fmt - '0';
-            }
+//             // Parse width
+//             for (; isdigit(*fmt); ++fmt) {
+//                 fmt_width *= 10;
+//                 fmt_width += *fmt - '0';
+//             }
 
-            switch (*fmt) {
-            case 's':
-                s = va_arg(vl, char*);
-                acnt += sizeof(char*);
-                // print_s(&buf, &i, s, fmt_width, fmt_flags);
-                break;
-            case 'd':
-                num = va_arg(vl, int);
-                acnt += sizeof(int);
-                // print_dec(&buf, &i, num, fmt_width, fmt_flags, 0);
-                break;
-            case 'x':
-                num = va_arg(vl, unsigned int);
-                acnt += sizeof(unsigned int);
-                // print_hex(&buf, &i, num, fmt_width, fmt_flags, 0);
-                break;
-            case 'X':
-                num = va_arg(vl, unsigned int);
-                acnt += sizeof(unsigned int);
-                // print_hex(&buf, &i, num, fmt_width, fmt_flags, 1);
-                break;
-            case 'u':
-                num = va_arg(vl, unsigned int);
-                acnt += sizeof(unsigned int);
-                // print_dec(&buf, &i, num, fmt_width, fmt_flags, 0);
-                break;
-                break;
-            case 'p':
-                num = va_arg(vl, uintptr_t);
-                acnt += sizeof(uintptr_t);
-                // print_hex(&buf, &i, num, fmt_width, fmt_flags, 0);
-                break;
-            case 'P':
-                num = va_arg(vl, uintptr_t);
-                acnt += sizeof(uintptr_t);
-                // print_hex(&buf, &i, num, fmt_width, fmt_flags, 1);
-                break;
-            case 'l':
-                if (*(fmt + 1) == 'l') {
-                    while (acnt & (sizeof(uint64_t) - 1)) {
-                        va_arg(vl, int);
-                        acnt += sizeof(int);
-                    }
-                    num = va_arg(vl, uint64_t);
-                    acnt += sizeof(uint64_t);
-                    ++fmt;
+//             switch (*fmt) {
+//             case 's':
+//                 s = va_arg(vl, char*);
+//                 acnt += sizeof(char*);
+//                 // print_s(&buf, &i, s, fmt_width, fmt_flags);
+//                 break;
+//             case 'd':
+//                 num = va_arg(vl, int);
+//                 acnt += sizeof(int);
+//                 // print_dec(&buf, &i, num, fmt_width, fmt_flags, 0);
+//                 break;
+//             case 'x':
+//                 num = va_arg(vl, unsigned int);
+//                 acnt += sizeof(unsigned int);
+//                 // print_hex(&buf, &i, num, fmt_width, fmt_flags, 0);
+//                 break;
+//             case 'X':
+//                 num = va_arg(vl, unsigned int);
+//                 acnt += sizeof(unsigned int);
+//                 // print_hex(&buf, &i, num, fmt_width, fmt_flags, 1);
+//                 break;
+//             case 'u':
+//                 num = va_arg(vl, unsigned int);
+//                 acnt += sizeof(unsigned int);
+//                 // print_dec(&buf, &i, num, fmt_width, fmt_flags, 0);
+//                 break;
+//                 break;
+//             case 'p':
+//                 num = va_arg(vl, uintptr_t);
+//                 acnt += sizeof(uintptr_t);
+//                 // print_hex(&buf, &i, num, fmt_width, fmt_flags, 0);
+//                 break;
+//             case 'P':
+//                 num = va_arg(vl, uintptr_t);
+//                 acnt += sizeof(uintptr_t);
+//                 // print_hex(&buf, &i, num, fmt_width, fmt_flags, 1);
+//                 break;
+//             case 'l':
+//                 if (*(fmt + 1) == 'l') {
+//                     while (acnt & (sizeof(uint64_t) - 1)) {
+//                         va_arg(vl, int);
+//                         acnt += sizeof(int);
+//                     }
+//                     num = va_arg(vl, uint64_t);
+//                     acnt += sizeof(uint64_t);
+//                     ++fmt;
 
-                } else {
-                }
-                // ...
-                break;
-            case 'c':
-                acnt += sizeof(int);
-                flush_buffer_if_overflow(buf, i, 1);
-                buf[i++] = (char)(va_arg(vl, int));
-                break;
-            }
-        } else {
-        out:
-            flush_buffer_if_overflow(buf, i, 1);
-            buf[i++] = *fmt;
-        }
-    }
-    flush_buffer_if_overflow(buf, i, CONSOLE_BUF_SIZE);
-}
+//                 } else {
+//                 }
+//                 // ...
+//                 break;
+//             case 'c':
+//                 acnt += sizeof(int);
+//                 flush_buffer_if_overflow(buf, i, 1);
+//                 buf[i++] = (char)(va_arg(vl, int));
+//                 break;
+//             }
+//         } else {
+//         out:
+//             flush_buffer_if_overflow(buf, i, 1);
+//             buf[i++] = *fmt;
+//         }
+//     }
+//     flush_buffer_if_overflow(buf, i, CONSOLE_BUF_SIZE);
+// }
 
 static int rt_vsnprintf(char* out, size_t n, const char* s, va_list vl)
 {
