7c3440fc8cc28a076e2a143a04da3a9b1162afba
Macromogic <tonyh0411@gmail.com>
2021-10-27 17:29:31 +0800

    debug: memory setup

--- emodules/rt_base/base_util/elf.c
+++ emodules/rt_base/base_util/elf.c
@@ -1,6 +1,7 @@
 #include "elf.h"
 #include "../m3/page_pool.h"
 #include "../m3/page_table.h"
+#include "../rt_console.h"
 
 static pte_attr_t get_pte_attr(word_t pflags)
 {
@@ -22,31 +23,35 @@ static int elf_check(ehdr_t* ehdr, size_t elf_size)
 
     // Check headers
     if (ident[EI_MAG0] != ELFMAG0 || ident[EI_MAG1] != ELFMAG1 || ident[EI_MAG2] != ELFMAG2 || ident[EI_MAG3] != ELFMAG3) {
-        // em_error("Not ELF file\n");
+        em_debug("ident @%lx\n", ident);
+        em_debug("%x %x %x %x\n", ident[EI_MAG0], ident[EI_MAG1], ident[EI_MAG1], ident[EI_MAG1]);
+        em_error("Not ELF file\n");
         goto error;
     }
     if (ident[EI_CLASS] != ELFCLASS) {
-        // em_error("Not %d-bits file\n", __risv_xlen);
+        em_error("Not %d-bits file\n", __riscv_xlen);
         goto error;
     }
     if (ehdr->e_type != ET_EXEC) {
-        // em_error("Not executable file\n");
+        em_error("Not executable file\n");
         goto error;
     }
     if (ehdr->e_machine != EM_RISCV) {
-        // em_error("Invalid machine platform\n");
+        em_error("Invalid machine platform\n");
         goto error;
     }
 
     // Check program header table
     ph_end = ehdr->e_phoff + ehdr->e_phentsize * ehdr->e_phnum;
     if (elf_size < ph_end || ph_end < ehdr->e_phoff) {
+        em_error("Program header error\n");
         goto error;
     }
 
     // Check section table
     sh_end = ehdr->e_shoff + ehdr->e_shentsize * ehdr->e_shnum;
     if (elf_size < sh_end || sh_end < ehdr->e_shoff) {
+        em_error("Section header error\n");
         goto error;
     }
 
@@ -59,12 +64,15 @@ error:
 static int elf_map(uintptr_t elf_addr)
 {
     ehdr_t* ehdr = (ehdr_t*)elf_addr;
-    phdr_t* phdr = (phdr_t*)(ehdr + ehdr->e_phoff);
+    phdr_t* phdr = (phdr_t*)(elf_addr + ehdr->e_phoff);
     uintptr_t va_start, pa_start, file_end, memory_end;
     size_t n_pages;
     unsigned int i;
 
+    em_debug("phoff=%d\n", ehdr->e_phoff);
+    em_debug("phnum = %d\n", ehdr->e_phnum);
     for (i = 0; i < ehdr->e_phnum; ++i) {
+        em_debug("phdr[%d].p_type: %d\n", i, phdr[i].p_type);
         if (phdr[i].p_type != PT_LOAD) {
             continue;
         }
@@ -73,6 +81,7 @@ static int elf_map(uintptr_t elf_addr)
         file_end = va_start + phdr[i].p_filesz;
         memory_end = va_start + phdr[i].p_memsz;
         n_pages = (PAGE_UP(file_end) - PAGE_DOWN(va_start)) >> EPAGE_SHIFT;
+        em_debug("Mapping %d page(s) from 0x%lx to 0x%lx\n", n_pages, PAGE_DOWN(pa_start), PAGE_DOWN(va_start));
         map_page(PAGE_DOWN(va_start), PAGE_DOWN(pa_start), n_pages,
             PTE_U | get_pte_attr(phdr[i].p_flags), 1);
         if (PAGE_UP(file_end) < memory_end) {
@@ -86,11 +95,14 @@ static int elf_map(uintptr_t elf_addr)
 
 uintptr_t elf_load(uintptr_t elf_addr, size_t elf_size, uintptr_t* usr_heap_top_addr)
 {
+    em_debug("Loading ELF at %lx with size=%d\n", elf_addr, elf_size);
     ehdr_t* ehdr = (ehdr_t*)elf_addr;
     if (elf_check(ehdr, elf_size)) {
+        em_error("ELF check failed\n");
         return -1;
     }
     if (elf_map(elf_addr)) {
+        em_error("ELF mapping failed\n");
         return -1;
     }
     *usr_heap_top_addr = EUSR_HEAP_START;
--- emodules/rt_base/rt_base.c
+++ emodules/rt_base/rt_base.c
@@ -49,11 +49,15 @@ static inline void attest_payload(void* payload_start, size_t payload_size)
 {
     uint64_t begin_cycle, end_cycle;
     uint8_t md2hash[MD2_BLOCK_SIZE];
+    int i;
 
     begin_cycle = read_csr(cycle);
     md2(payload_start, payload_size, md2hash);
     end_cycle = read_csr(cycle);
-    em_debug("MD2: 0x%x in %ld cycles\n", end_cycle - begin_cycle);
+    em_debug("MD2: in %ld cycles\n", end_cycle - begin_cycle);
+    for (i = 0; i < MD2_BLOCK_SIZE; ++i) {
+        em_debug("%x\n", md2hash[i]);
+    }
 }
 
 // Memory mapping setup
@@ -62,26 +66,26 @@ static void init_map_alloc_pages(drv_addr_t* drv_list,
     uintptr_t usr_avail_start, size_t usr_avail_size,
     uintptr_t base_avail_start, size_t base_avail_size)
 {
-    uintptr_t drv_pa_start, drv_pa_end;
-    size_t n_drv_pages;
+    // uintptr_t drv_pa_start, drv_pa_end;
+    // size_t n_drv_pages;
     uintptr_t usr_stack_start;
     size_t n_usr_stack_pages, n_base_stack_pages;
     uintptr_t drv_sp;
 
-    // Drivers
-    em_debug("drv_list = 0x%lx\n", drv_list);
-    if (drv_list[0].drv_start) {
-        drv_pa_start = PAGE_DOWN(drv_list[0].drv_start - enc_va_pa_offset);
-        drv_pa_end = PAGE_UP(((uintptr_t)drv_list) + MAX_DRV * sizeof(drv_addr_t));
-        n_drv_pages = (drv_pa_end - drv_pa_start) >> EPAGE_SHIFT;
-        em_debug("drv_pa_end = 0x%x drv_pa_start = 0x%x\n", drv_pa_end,
-            drv_pa_start);
-        em_debug("n_drv_pages = %d\n", n_drv_pages);
-        map_page(PAGE_DOWN(drv_list[0].drv_start), drv_pa_start,
-            n_drv_pages, PTE_V | PTE_R | PTE_X | PTE_W, 0);
-        em_debug("\033[1;33mdrv: 0x%x - 0x%x -> 0x%x\n\033[0m",
-            drv_pa_start, drv_pa_end, __pa(drv_pa_start));
-    }
+    // TODO Drivers
+    // em_debug("drv_list = 0x%lx\n", drv_list);
+    // if (drv_list[0].drv_start) {
+    //     drv_pa_start = PAGE_DOWN(drv_list[0].drv_start - enc_va_pa_offset);
+    //     drv_pa_end = PAGE_UP(((uintptr_t)drv_list) + MAX_DRV * sizeof(drv_addr_t));
+    //     n_drv_pages = (drv_pa_end - drv_pa_start) >> EPAGE_SHIFT;
+    //     em_debug("drv_pa_end = 0x%x drv_pa_start = 0x%x\n", drv_pa_end,
+    //         drv_pa_start);
+    //     em_debug("n_drv_pages = %d\n", n_drv_pages);
+    //     map_page(PAGE_DOWN(drv_list[0].drv_start), drv_pa_start,
+    //         n_drv_pages, PTE_V | PTE_R | PTE_X | PTE_W, 0);
+    //     em_debug("\033[1;33mdrv: 0x%x - 0x%x -> 0x%x\n\033[0m",
+    //         drv_pa_start, drv_pa_end, __pa(drv_pa_start));
+    // }
 
     // Remaining user memory
     map_page(enc_va_pa_offset + usr_avail_start, usr_avail_start,
@@ -106,7 +110,7 @@ static void init_map_alloc_pages(drv_addr_t* drv_list,
     map_page(enc_va_pa_offset + page_table_start, page_table_start,
         (page_table_size) >> EPAGE_SHIFT, PTE_V | PTE_W | PTE_R, 0);
     em_debug("page_table and trie: 0x%x - 0x%x -> 0x%x\n", page_table_start,
-        page_table_size, __pa(page_table_start));
+        page_table_start + page_table_size, __pa(page_table_start));
     // `.rodata', `.bss', `.init.data`, `.data' sections
     MAP_BASE_SECTION(rodata, PTE_V | PTE_R);
     MAP_BASE_SECTION(bss, PTE_V | PTE_W | PTE_R);
@@ -167,55 +171,65 @@ void init_mem(uintptr_t _, uintptr_t id, uintptr_t payload_pa_start,
     uintptr_t usr_avail_start, usr_avail_size;
     uintptr_t satp, sstatus;
     uintptr_t usr_pc;
-    int i;
+    // int i;
     uintptr_t usr_sp = EUSR_STACK_TOP;
     uintptr_t drv_sp = ERT_STACK_TOP;
 
+    extern uintptr_t _start;
+    em_debug("_start = 0x%lx\n", &_start);
+    em_debug("_ = 0x%lx\n", _);
+
     // Update VA/PA offset
-    enc_va_pa_offset = ERT_VA_START - payload_pa_start;
-    em_debug("\033[1;33moffset: 0x%x\n\033[0m", enc_va_pa_offset);
+    enc_va_pa_offset = ERT_VA_START - (uintptr_t)&_start;
+    em_debug("\033[1;33moffset: 0x%lx\n\033[0m", enc_va_pa_offset);
     // `va_top' will increase by EMEM_SIZE after `page_pool_init'
-    va_top = ERT_VA_START;
+    va_top = ERT_VA_START + PARTITION_SIZE; // Resv for base module
     attest_payload((void*)payload_pa_start, payload_size);
     enclave_id = id;
 
     // Transform driver addresses into VA
-    em_debug("drv_list[0].drv_start = 0x%x\n", drv_list[0].drv_start);
-    for (i = 0; i < MAX_DRV && drv_list[i].drv_start; i++) {
-        drv_list[i].drv_start += enc_va_pa_offset;
-        // drv_list[i].drv_end += enc_va_pa_offset;
-        // em_debug("drv_list[%d].drv_start: 0x%x, drv_end: 0x%x\n", i,
-        //     drv_list[i].drv_start, drv_list[i].drv_end);
-    }
-    drv_addr_list = (drv_addr_t*)((void*)drv_list + enc_va_pa_offset);
+    em_debug("drv_list[0].drv_start = 0x%lx\n", drv_list[0].drv_start);
+    // TODO drivers
+    // for (i = 0; i < MAX_DRV && drv_list[i].drv_start; i++) {
+    //     drv_list[i].drv_start += enc_va_pa_offset;
+    //     // drv_list[i].drv_end += enc_va_pa_offset;
+    //     // em_debug("drv_list[%d].drv_start: 0x%x, drv_end: 0x%x\n", i,
+    //     //     drv_list[i].drv_start, drv_list[i].drv_end);
+    // }
+    // drv_addr_list = (drv_addr_t*)((void*)drv_list + enc_va_pa_offset);
 
     // Setup page table, trie and base memory space
-    page_table_start = PAGE_UP((uintptr_t)drv_list + MAX_DRV * sizeof(drv_addr_t));
+    page_table_start = PAGE_UP((uintptr_t)drv_list /* + MAX_DRV * sizeof(drv_addr_t)*/);
     page_table_size = PAGE_UP(PAGE_DIR_POOL * EPAGE_SIZE);
-    em_debug("page_table_start = 0x%x\n", page_table_start);
-    em_debug("page_table_size = 0x%x\n", page_table_size);
+    em_debug("page_table_start = 0x%llx\n", page_table_start);
+    em_debug("page_table_size = 0x%llx\n", page_table_size);
     pt_root_pa = page_table_start;
 
     trie_start = page_table_start + page_table_size;
     trie_size = PAGE_UP(sizeof(trie_t));
 
     base_avail_start = trie_start + trie_size;
-    base_avail_size = PAGE_DOWN(payload_pa_start + EMEM_SIZE - base_avail_start);
-    em_debug("base_avail_start = 0x%x\n", base_avail_start);
-    em_debug("base_avail_size = %x\n", base_avail_size);
+    base_avail_size = PAGE_DOWN(PARTITION_UP(base_avail_start) - base_avail_start);
+    em_debug("base_avail_start = 0x%lx\n", base_avail_start);
+    em_debug("base_avail_size = %lx\n", base_avail_size);
     page_pool_init(base_avail_start, base_avail_size, IDX_RT);
 
     // Setup user memory space
     usr_avail_start = PAGE_UP(payload_pa_start + payload_size);
-    usr_avail_size = PAGE_DOWN(EUSR_MEM_SIZE - payload_size);
+    usr_avail_size = PAGE_DOWN(PARTITION_SIZE - payload_size % PARTITION_SIZE);
     page_pool_init(usr_avail_start, usr_avail_size, IDX_USR);
-    em_debug("Initializing user page pool: 0x%x, size: 0x%x\n",
+    em_debug("Initializing user page pool: 0x%llx, size: 0x%llx\n",
         usr_avail_start, usr_avail_size);
     em_debug("User page pool initialization done\n");
     // check_pte_all_zero(); // Is it necessary?
-    em_debug("\033[1;33mroot: 0x%x\n\033[0m", get_pt_root());
+    em_debug("\033[1;33mroot: 0x%llx\n\033[0m", get_pt_root());
 
     usr_pc = elf_load(payload_pa_start, payload_size, &usr_heap_top);
+    if (usr_pc == -1) {
+        em_error("ELF load error\n");
+    } else {
+        em_debug("ELF load done\n");
+    }
 
     init_map_alloc_pages(drv_list, page_table_start,
         page_table_size + trie_size, usr_avail_start,
@@ -231,9 +245,12 @@ void init_mem(uintptr_t _, uintptr_t id, uintptr_t payload_pa_start,
 
     // Inform M-mode of locations of page table and inverse mapping
     ecall_map_register(&pt_root_pa, &inv_map, &enc_va_pa_offset);
+    em_debug("After ecall map_register\n");
     va_top += EMEM_SIZE;
     // flush_dcache_range(payload_pa_start, payload_pa_start + EMEM_SIZE);
-    // asm volatile("fence rw, rw");
+    asm volatile("fence rw, rw");
+    flush_tlb();
+    em_debug("End of init_mem\n");
 
     asm volatile("mv a0, %0" ::"r"((uintptr_t)(satp)));
     asm volatile("mv a1, %0" ::"r"((uintptr_t)(drv_sp)));
@@ -245,7 +262,9 @@ void init_mem(uintptr_t _, uintptr_t id, uintptr_t payload_pa_start,
 
 void prepare_boot(uintptr_t usr_pc, uintptr_t usr_sp)
 {
+    em_debug("Begin prepare_boot\n");
     init_drivers();
+    em_debug("After init_drivers\n");
 
     // Allow S-mode to access U-mode memory
     uintptr_t sstatus = read_csr(sstatus);
@@ -262,3 +281,21 @@ void prepare_boot(uintptr_t usr_pc, uintptr_t usr_sp)
     write_csr(sepc, usr_pc);
     write_csr(sscratch, usr_sp);
 }
+
+#ifdef EMODULES_DEBUG
+void show_csr(void)
+{
+    uintptr_t sstatus, sepc, stvec, satp;
+    em_debug("------------------\n");
+    sstatus = read_csr(sstatus);
+    sepc = read_csr(sepc);
+    stvec = read_csr(stvec);
+    satp = read_csr(satp);
+
+    em_debug("status: 0x%lx\n", sstatus);
+    em_debug("sepc: 0x%lx\n", sepc);
+    em_debug("stvec: 0x%lx\n", stvec);
+    em_debug("satp: 0x%lx\n", satp);
+    em_debug("------------------\n");
+}
+#endif
