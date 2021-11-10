32531f641b8a34c04ad0129a92776880f6370e81
MstMoonshine <11710919@mail.sustech.edu.cn>
2021-11-10 19:05:54 +0800

    feat: dma registers map, dma status query

--- emodules/rt_base/dma_tmp/dma.c
+++ emodules/rt_base/dma_tmp/dma.c
@@ -1,7 +1,27 @@
 #include "dma.h"
-#include "../rt_console.h" 
+#include "../rt_console.h"
+#include "../m3/page_table.h"
+
+
+static void dma_control_page_mapping()
+{
+	uintptr_t addr = PDMA_BASE_ADDR;
+	uintptr_t n_pages = PDMA_SIZE >> EPAGE_SHIFT;
+	map_page(addr, addr, n_pages, PTE_V | PTE_W | PTE_R | PTE_D, 0);
+}
+
+static void print_dma_status()
+{
+	for (int i = 0; i < PDMA_NUM; i++) {
+		dma_control ctrl = *(dma_control *)PDMA_CONTROL(i);
+		em_debug("ID: %d\n", i);
+		em_debug("claim: %d\n", ctrl.claim);
+		em_debug("run: %d\n", ctrl.run);
+	}
+}
 
 void dma_debug()
 {
-	em_debug("hello world\n");
+	dma_control_page_mapping();
+	print_dma_status();
 }
