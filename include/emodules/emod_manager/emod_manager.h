fa5d3dd8aa4a4b205f914d715787b40bb6a2ad88
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-09 00:09:36 +0800

    feat: emod_manager struct and api

--- include/emodules/emod_manager/emod_manager.h
+++ include/emodules/emod_manager/emod_manager.h
@@ -11,8 +11,28 @@
 #pragma once
 
 #include <emodules/emodule_id.h>
+#include <emodules/emodule_desc.h>
 #include <types.h>
 
 #define MESSAGE_LOAD_MODULE	0x717DB77A
 
+typedef struct {
+	// test
+	void (*test)(void);
+
+	// emodule management
+	vaddr_t (*acquire_emodule)(u32 emod_id);
+
+	// memory management
+	int (*memory_map)(vaddr_t vaddr, paddr_t paaddr, usize len);
+
+	// ... to be determined
+} emod_manager_api_t;
+
+typedef struct {
+	emod_desc_t emod_manager_desc;
+	emod_manager_api_t emod_manager_api;
+} emod_manager_t;
+
+void emod_manager_init();
 void emod_manager_test();
