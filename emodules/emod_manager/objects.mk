f3d1a636158986996941388c272ab0c0dffba46f
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-09 00:11:02 +0800

    chores: build system

--- emodules/emod_manager/objects.mk
+++ emodules/emod_manager/objects.mk
@@ -2,7 +2,9 @@ emodule-objs-y =
 emodule-objs-y += entry.o 
 emodule-objs-y += emod_manager.o
 emodule-objs-y += boot/boot.o
+emodule-objs-y += memory/memory.o
 emodule-objs-y += panic/panic.o
+emodule-objs-y += emod_table/emod_table.o
 emodule-objs-y += printf/printf.o
 emodule-objs-y += debug/debug.o
 
