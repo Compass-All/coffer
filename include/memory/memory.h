67e2847b4c941165f38bbc1352ad26bf2a609d8d
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-15 00:04:48 +0800

    feat: memory util (util/memory.h to be removed)

---
+++ include/memory/memory.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <types.h>
+
+#ifndef ROUNDUP
+#define ROUNDUP(a, b) ((((a)-1) / (b) + 1) * (b))
+#endif
+
+#define PAGE_SHIFT 12
+#define PAGE_SIZE (1UL << PAGE_SHIFT)
+
+#define PAGE_UP(addr) (ROUNDUP(addr, PAGE_SIZE))
+#define PAGE_DOWN(addr) ((addr) & (~((PAGE_SIZE)-1)))
+
+#define PARTITION_SHIFT 21 // 2 MB for each partition
+#define PARTITION_SIZE (1UL << PARTITION_SHIFT)
+
+#define PARTITION_UP(addr) (ROUNDUP(addr, PARTITION_SIZE))
+#define PARTITION_DOWN(addr) ((addr) & (~((PARTITION_SIZE)-1)))
