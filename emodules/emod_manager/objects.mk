3a7d4c5b9b5b4bb8b98c00767076c531b37d50d3
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-15 00:05:45 +0800

    chores: page pool and page table target

--- emodules/emod_manager/objects.mk
+++ emodules/emod_manager/objects.mk
@@ -3,9 +3,12 @@ emodule-objs-y += entry.o
 emodule-objs-y += emod_manager.o
 emodule-objs-y += boot/boot.o
 emodule-objs-y += memory/memory.o
+emodule-objs-y += memory/page_pool.o
+emodule-objs-y += memory/page_table.o
 emodule-objs-y += panic/panic.o
 emodule-objs-y += emod_table/emod_table.o
 emodule-objs-y += printf/printf.o
 emodule-objs-y += debug/debug.o
 
 emodule-genflags = -DEMODULES_DEBUG=$(EMODULES_DEBUG)
+emodule-genflags += -DEMOD_MANAGER_SVA_START=$(EMOD_MANAGER_SVA_START)
