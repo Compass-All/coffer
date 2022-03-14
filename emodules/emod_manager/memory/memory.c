9a03c3227defaa3b800aaa14554135ada7ea835e
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-15 00:03:13 +0800

    feat: memory management

--- emodules/emod_manager/memory/memory.c
+++ emodules/emod_manager/memory/memory.c
@@ -1,5 +1,25 @@
 #include "memory.h"
 
+// initialized during boot
+volatile 	static paddr_t emod_manager_pa_start;
+// defined in config.mk
+const 		static vaddr_t emod_manager_va_start = EMOD_MANAGER_SVA_START;
+
+void set_emod_manager_pa_start(paddr_t pa_start)
+{
+	emod_manager_pa_start = pa_start;
+}
+
+paddr_t get_emod_manager_pa_start()
+{
+	return emod_manager_pa_start;
+}
+
+vaddr_t get_emod_manager_va_start()
+{
+	return emod_manager_va_start;
+}
+
 void wait_until_non_zero(volatile u64 *ptr)
 {
 	while (*ptr == 0UL);
--- emodules/emod_manager/memory/memory.h
+++ emodules/emod_manager/memory/memory.h
@@ -2,4 +2,7 @@
 
 #include <types.h>
 
+void set_emod_manager_pa_start(paddr_t pa_start);
+paddr_t get_emod_manager_pa_start();
+vaddr_t get_emod_manager_va_start();
 void wait_until_non_zero(volatile u64 *ptr);
