5796301c52f1bd278ca27fde6b5427ee37d36abd
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-16 19:00:43 +0800

    feat: dump reg in trap handler

--- emodules/emod_manager/trap/exceptions.c
+++ emodules/emod_manager/trap/exceptions.c
@@ -1,9 +1,32 @@
 #include "exceptions.h"
 #include <types.h>
 #include "../panic/panic.h"
+#include "../debug/debug.h"
 #include <util/gnu_attribute.h>
+#include <util/register.h>
 
-__diverge void exception_handler()
+__diverge void exception_handler(
+	u64* 	regs,
+	u64		sepc,
+	u64		scause,
+	u64		stval
+)
 {
-	panic("Trapped!");
+	error("Trapped! Exception!\n");
+
+	printf("sepc:\t0x%016x\t", sepc);
+	printf("scause:\t0x%016x\t", scause);
+	printf("stval:\t0x%016x\t", stval);
+	printf("\n");
+
+	printf("Register Dump:\n");
+	for (int i = 1; i < CTX_INDEX_MAX; i++) {
+		printf("x%d:\t0x%016lx\t", i, regs[i]);
+
+		if (i % 3 == 0)
+			printf("\n");
+	}
+	printf("\n");
+
+	panic("\n");
 }
