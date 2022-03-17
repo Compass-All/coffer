f9be2bf70c152049eeffd7ba13c6ba8b189e82a2
MstMoonshine <11710919@mail.sustech.edu.cn>
2022-03-17 22:52:25 +0800

    test: load emodule after mmu gets turned on

--- emodules/emod_manager/emod_manager.c
+++ emodules/emod_manager/emod_manager.c
@@ -7,6 +7,7 @@
 #include "debug/debug.h"
 #include "panic/panic.h"
 #include "memory/memory.h"
+#include "memory/page_table.h"
 #include "emod_table/emod_table.h"
 
 #include <emodules/emod_dummy/emod_dummy.h>
@@ -45,6 +46,7 @@ void emod_manager_init()
 	emod_manager_api.test = api_test;
 	// ...
 	// todo!
+	show(emod_manager_api.test);
 
 	// init emod_manager
 	emod_manager.emod_manager_desc = emod_manager_desc;
@@ -56,10 +58,10 @@ void emod_manager_init()
 
 // ----- temporary implmentation -----
 
+#define EMOD_DEBUG_LEN	0x3000
 #define EMOD_DUMMY_LEN	0x5000
-#define EMOD_DEBUG_LEN	0x6000
-u8 emod_dummy_buffer[EMOD_DUMMY_LEN];
-u8 emod_debug_buffer[EMOD_DEBUG_LEN];
+__page_aligned u8 emod_debug_buffer[EMOD_DEBUG_LEN];
+__page_aligned u8 emod_dummy_buffer[EMOD_DUMMY_LEN];
 
 static void load_emod_debug()
 {
@@ -73,7 +75,7 @@ static void load_emod_debug()
 		(vaddr_t)&message_load_debug,
 		sizeof(message_load_debug)
 	);
-	debug("[load_emod_debug] send_ret = 0x%lx\n", send_ret);
+	debug("send_ret = 0x%lx\n", send_ret);
 
 	if (send_ret) {
 		panic("send_ret error\n");
@@ -84,7 +86,7 @@ static void load_emod_debug()
 		(vaddr_t)&emod_debug_buffer,
 		sizeof(emod_debug_buffer)
 	);
-	debug("[load_emod_debug] listen_ret = 0x%lx\n", listen_ret);
+	debug("listen_ret = 0x%lx\n", listen_ret);
 
 	if (listen_ret) {
 		panic("listen_ret error\n");
@@ -93,7 +95,15 @@ static void load_emod_debug()
 	__ecall_ebi_suspend();
 	wait_until_non_zero((volatile u64 *)&emod_debug_buffer);
 
-	hexdump((vaddr_t)&emod_debug_buffer, 0x10);
+	show(&emod_debug_buffer);
+
+	for (int i = 0; i < EMOD_DEBUG_LEN / PAGE_SIZE; i++) {
+		vaddr_t vaddr = (vaddr_t)&emod_debug_buffer + i * PAGE_SIZE;
+		map_page(vaddr, vaddr - get_va_pa_offset(),
+			PTE_R | PTE_W | PTE_X, SV39_LEVEL_PAGE);
+	}
+
+	hexdump((vaddr_t)0xffffffe14c407c00, 0x200);
 
 	vaddr_t (*debug_init)(vaddr_t) = (void *)&emod_debug_buffer;
 	vaddr_t debug_module_getter_addr = debug_init((vaddr_t)get_emodule);
@@ -101,67 +111,16 @@ static void load_emod_debug()
 	register_emodule(EMODULE_ID_DEBUG, debug_module_getter_addr);
 }
 
-static void load_emod_dummy()
-{
-	u32 message_load_dummy[2] = {
-		MESSAGE_LOAD_MODULE,
-		EMODULE_ID_DUMMY
-	};
-
-	u64 send_ret = __ecall_ebi_send_message(
-		HOST_EID,
-		(vaddr_t)&message_load_dummy,
-		sizeof(message_load_dummy)
-	);
-	debug("[load_emod_dummy] send_ret = 0x%lx\n", send_ret);
-
-	if (send_ret) {
-		panic("send_ret error\n");
-	}
-
-	u64 listen_ret = __ecall_ebi_listen_message(
-		HOST_EID,
-		(vaddr_t)&emod_dummy_buffer,
-		sizeof(emod_dummy_buffer)
-	);
-	debug("[load_emod_dummy] listen_ret = 0x%lx\n", listen_ret);
-
-	if (listen_ret) {
-		panic("listen_ret error\n");
-	}
-
-	__ecall_ebi_suspend();
-	wait_until_non_zero((volatile u64 *)&emod_dummy_buffer);
-
-	hexdump((vaddr_t)&emod_dummy_buffer, 0x10);
-
-	vaddr_t (*dummy_init)(vaddr_t) = (void *)&emod_dummy_buffer;
-	vaddr_t dummy_module_getter_addr = dummy_init((vaddr_t)get_emodule);
-
-	register_emodule(EMODULE_ID_DUMMY, dummy_module_getter_addr);
-}
-
 void emod_manager_test()
 {
 	load_emod_debug();
-	load_emod_dummy();
-
-	vaddr_t dummy_getter_addr = acquire_emodule(EMODULE_ID_DUMMY);
-	emod_dummy_t (*get_emod_dummy)(void) = (void *)dummy_getter_addr;
-	emod_dummy_t emod_dummy = get_emod_dummy();
-	int one_plus_two = emod_dummy.emod_dummy_api.dummy_func1(1, 2);
-	int expected = 3;
-	assert((void *)&one_plus_two, (void *)&expected, sizeof(int));
-
-	debug("emod dummy id: %u\n", emod_dummy.emod_dummy_desc.emod_id);
-	debug("emod name: %s\n", emod_dummy.emod_dummy_desc.name);
 
 	vaddr_t debug_getter_addr = acquire_emodule(EMODULE_ID_DEBUG);
 	emod_debug_t (*get_emod_debug)(void) = (void *)debug_getter_addr;
 	emod_debug_t emod_debug = get_emod_debug();
 
 	emod_debug.emod_debug_api.printd("Hello world from printd\n");
-	emod_debug.emod_debug_api.printd("Int test: %d\n", expected);
+	emod_debug.emod_debug_api.printd("Int test: %d\n", 4);
 	emod_debug.emod_debug_api.hexdump(debug_getter_addr, 0x10);
 
 	return;
