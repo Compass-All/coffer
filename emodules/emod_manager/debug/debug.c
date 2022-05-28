81699d21c12c94f6209c7b1957d6ccf1ef8b0f3d
MstMoonshine <mingde-ren@outlook.com>
2022-05-28 19:41:03 +0800

    feat: hexdump not panic for invalid va

--- emodules/emod_manager/debug/debug.c
+++ emodules/emod_manager/debug/debug.c
@@ -1,18 +1,28 @@
 #include "debug.h"
 #include "../panic/panic.h"
+#include "../memory/page_table.h"
 
 void hexdump(vaddr_t addr, usize len)
 {
 	const u8 group_size = 4;
 	u32 *ptr = (u32 *)addr;
+
+	paddr_t pa = get_pa(addr);	
+	if (!pa) {
+		printf("va not valid\n");
+		return;
+	}
+
+	printf("##### start of hexdump, pa = 0x%lx\n", pa);
 	for (int i = 0; i < len; i += group_size * sizeof(u32),
 		ptr += group_size) {
 
-		printf("%p:\t", ptr);
+		printf("[hexdump] 0x%p:\t", ptr);
 		for (int j = 0; j < group_size - 1; j++)
 			printf("0x%08x\t", ptr[j]);
 		printf("0x%08x\n", ptr[group_size - 1]);
 	}
+	printf("##### end of hexdump\n");
 }
 
 void assert(u8 *ptr1, u8 *ptr2, usize len)
