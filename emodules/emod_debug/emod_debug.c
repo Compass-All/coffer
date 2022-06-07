ca8505cb95f36eb53c88f99e0cbb5819b28ad0cc
MstMoonshine <mingde-ren@outlook.com>
2022-06-07 16:22:03 +0800

    style: rename

--- emodules/emod_debug/emod_debug.c
+++ emodules/emod_debug/emod_debug.c
@@ -16,9 +16,9 @@ static emod_debug_t emod_debug;
 
 // printf from "printf.h"
 #ifdef EMODULES_DEBUG
-#define printf(fmt, ...) printf_(fmt, ##__VA_ARGS__)
+#define debug(fmt, ...) printf_(fmt, ##__VA_ARGS__)
 #else
-#define printf(fmt, ...)
+#define debug(fmt, ...)
 #endif
 
 __unused static int dummy_printd(const char* format, ...)
@@ -33,38 +33,38 @@ static void hexdump(vaddr_t addr, usize len)
 
 	paddr_t pa = get_pa(addr);	
 	if (!pa) {
-		printf("va not valid\n");
+		debug("va not valid\n");
 		return;
 	}
 
-	printf("##### start of hexdump, pa = 0x%lx\n", pa);
+	debug("##### start of hexdump, pa = 0x%lx\n", pa);
 	for (int i = 0; i < len; i += group_size * sizeof(u32),
 		ptr += group_size) {
 
-		printf("[hexdump] 0x%p:\t", ptr);
+		debug("[hexdump] 0x%p:\t", ptr);
 		for (int j = 0; j < group_size - 1; j++)
-			printf("0x%08x\t", ptr[j]);
-		printf("0x%08x\n", ptr[group_size - 1]);
+			debug("0x%08x\t", ptr[j]);
+		debug("0x%08x\n", ptr[group_size - 1]);
 	}
-	printf("##### end of hexdump\n");
+	debug("##### end of hexdump\n");
 }
 
 static void assert(u8 *ptr1, u8 *ptr2, usize len)
 {
 	if (!ptr1 || !ptr2) {
-		printf("NULL Pointer during assertion\n");
+		debug("NULL Pointer during assertion\n");
 	}
 	for (int i = 0; i < len; i++) {
 		if (*ptr1 != *ptr2) {
-			printf("[assert] 0x%x\t(at 0x%p)\t!= 0x%x\t(at 0x%p)\n",
+			debug("[assert] 0x%x\t(at 0x%p)\t!= 0x%x\t(at 0x%p)\n",
 				*ptr1, ptr1, *ptr2, ptr2);
-			printf("[assert] Assert failed\n");
+			debug("[assert] Assert failed\n");
 			while (1);
 		}
 		ptr1++;
 		ptr2++;
 	}
-	printf("[assert] Assert passed!\n");
+	debug("[assert] Assert passed!\n");
 }
 
 // Emodule Init and Getter
@@ -82,6 +82,7 @@ vaddr_t debug_init(vaddr_t emod_manager_getter)
 #else
 		.printd = dummy_printd,
 #endif
+		.printf = printf_,
 		.hexdump = hexdump,
 		.assert = assert
 	};
@@ -91,11 +92,11 @@ vaddr_t debug_init(vaddr_t emod_manager_getter)
 		.emod_debug_api = emod_debug_api
 	};
 
-	printf("[debug_init] emod_manager_getter at 0x%lx\n",
+	debug("[debug_init] emod_manager_getter at 0x%lx\n",
 		emod_manager_getter);
 
 	if (emod_manager_getter == (vaddr_t)0UL) {
-		printf("Error: NULL emod_manager getter\n");
+		debug("Error: NULL emod_manager getter\n");
 		return (vaddr_t)0UL;
 	}
 
