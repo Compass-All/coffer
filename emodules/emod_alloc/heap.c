69adc477de92929f342a5b283b4c288c215112b7
MstMoonshine <mingde-ren@outlook.com>
2022-06-08 16:45:30 +0800

    feat: comment new memset impl

--- emodules/emod_alloc/heap.c
+++ emodules/emod_alloc/heap.c
@@ -156,8 +156,38 @@ static void *memset(void *s, int c, usize count)
 		*temp++ = c;
 	}
 
-	return s;
+    return s;
 }
+
+// static void *memset(void* dst, int byte, size_t len)
+// {
+//     size_t offset = 0;
+//     uint64_t fill = byte;
+//     fill |= fill << 8;
+//     fill |= fill << 16;
+//     fill |= fill << 32;
+//     while (len > 0) {
+//         if (len > 8) {
+//             *((uint64_t*)(dst + offset)) = fill;
+//             offset += 8;
+//             len -= 8;
+//         } else if (len > 4) {
+//             *((uint32_t*)(dst + offset)) = fill & 0xFFFFFFFF;
+//             offset += 4;
+//             len -= 4;
+//         } else if (len > 2) {
+//             *((uint16_t*)(dst + offset)) = fill & 0xFFFF;
+//             offset += 2;
+//             len -= 2;
+//         } else {
+//             *((uint8_t*)(dst + offset)) = byte;
+//             ++offset;
+//             --len;
+//         }
+//     }
+
+// 	return dst;
+// }
 
 void *heap_calloc(size_t number, size_t size)
 {
