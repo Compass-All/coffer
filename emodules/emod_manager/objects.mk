e9c948614df6948ecdeec44af5551c53389f056b
Mingde Ren <mingde-ren@outlook.com>
2022-07-12 23:29:51 +0800

    feat: initial ecc files

--- emodules/emod_manager/objects.mk
+++ emodules/emod_manager/objects.mk
@@ -2,6 +2,8 @@ emodule-objs-y =
 emodule-objs-y += entry.o 
 emodule-objs-y += emod_manager.o
 emodule-objs-y += boot/boot.o
+emodule-objs-y += attest/ecc.o
+emodule-objs-y += attest/md2.o
 emodule-objs-y += memory/memory.o
 emodule-objs-y += memory/page_pool.o
 emodule-objs-y += memory/page_table.o
@@ -11,6 +13,7 @@ emodule-objs-y += trap/exceptions.o
 emodule-objs-y += trap/syscalls.o
 emodule-objs-y += emod_table/emod_table.o
 emodule-objs-y += printf/printf.o
+emodule-objs-y += util/string.o
 emodule-objs-y += debug/debug.o
 
 emodule-genflags = 
