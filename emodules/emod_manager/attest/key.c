58fe432bdd4a48ae3f825951025e833448f5eb48
Mingde Ren <mingde-ren@outlook.com>
2022-07-19 16:37:46 +0800

    feat: key generetor

---
+++ emodules/emod_manager/attest/key.c
@@ -0,0 +1,5 @@
+#include "key.h"
+#include <types.h>
+
+const u8 pub_key[] = { ATTEST_PUB_KEY };
+// const u8 pri_key[] = { ATTEST_PRI_KEY };
---
+++ emodules/emod_manager/attest/key.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <types.h>
+
+extern const u8 pub_key[];
+// extern const u8 pri_key[];
