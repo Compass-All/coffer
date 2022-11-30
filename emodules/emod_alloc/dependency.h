12546a3cd11f7adec01f4de480ac25a109ffaa8d
MstMoonshine <mingde-ren@outlook.com>
2022-12-01 00:11:40 +0800

    feat: add color

--- emodules/emod_alloc/dependency.h
+++ emodules/emod_alloc/dependency.h
@@ -4,6 +4,16 @@
 #include <emodules/emod_manager/emod_manager.h>
 #include <emodules/emod_debug/emod_debug.h>
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[31m"
+#define KGRN  "\x1B[32m"
+#define KYEL  "\x1B[33m"
+#define KBLU  "\x1B[34m"
+#define KMAG  "\x1B[35m"
+#define KCYN  "\x1B[36m"
+#define KWHT  "\x1B[37m"
+#define RESET "\033[0m"
+
 extern emod_manager_t 	emod_manager;
 extern emod_debug_t 	emod_debug;
 
@@ -14,7 +24,9 @@ extern emod_debug_t 	emod_debug;
 #define printf		emod_debug.emod_debug_api.printf
 #define hexdump		emod_debug.emod_debug_api.hexdump
 #define debug(fmt, ...) \
-	printd("\033[37m[%s] " fmt "\033[0m", __func__, ##__VA_ARGS__)
+	printd(KWHT "[%s] " fmt RESET, __func__, ##__VA_ARGS__)
+#define info(fmt, ...) \
+	printd(KMAG "[%s] " fmt RESET, __func__, ##__VA_ARGS__)
 #define show(v) 	debug(#v "\t=\t0x%lx\n", (v))
 #define LOG(v)		printf("[%s]" #v "\t=\t0x%lx\n", __func__, (v))
 
