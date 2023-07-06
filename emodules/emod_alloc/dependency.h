1a06bd99ab84cbbecba5e95d5304bdfde1bfcd14
MstMoonshine <mingde-ren@outlook.com>
2023-07-06 17:30:49 +0800

    emod_alloc: feat: assert

--- emodules/emod_alloc/dependency.h
+++ emodules/emod_alloc/dependency.h
@@ -12,12 +12,13 @@ extern emod_debug_t 	emod_debug;
 #define get_pa 		emod_manager.emod_manager_api.get_pa
 
 // #define printd		emod_debug.emod_debug_api.printd
-#define printd(fmt, ...)		
 #define printf		emod_debug.emod_debug_api.printf
 #define hexdump		emod_debug.emod_debug_api.hexdump
 
 // #define debug(fmt, ...) printd(KWHT "[%s] " fmt RESET, __func__, ##__VA_ARGS__)
 // #define info(fmt, ...) printd(KMAG "[%s] " fmt RESET, __func__, ##__VA_ARGS__)
+
+#define printd(fmt, ...)		
 #define debug(fmt, ...)
 #define info(fmt, ...)
 
@@ -25,3 +26,11 @@ extern emod_debug_t 	emod_debug;
 #define LOG(v)		printf("[%s]" #v "\t=\t0x%lx\n", __func__, (v))
 
 #define panic	emod_manager.emod_manager_api.panic
+
+#define ASSERT(x) \
+    do { \
+        if (!(x)) { \
+            printf("[%s: %u] Assertion failed: %s\n", __FILE__, __LINE__, #x); \
+            panic("Panic\n"); \
+        } \
+    } while (0)
