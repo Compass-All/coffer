789b803dacb996b4266c8725e96dc239be2803ef
MstMoonshine <mingde-ren@outlook.com>
2023-12-21 14:28:06 +0800

    fix: decrease enclave/thread number

--- include/enclave/threads.h
+++ include/enclave/threads.h
@@ -3,12 +3,12 @@
 #if defined __QEMU__
 #define NUM_ENCLAVE     64
 #elif defined __UNMATCHED__
-#define NUM_ENCLAVE		64
+#define NUM_ENCLAVE		32
 #else
 #error "Unknown Platform"
 #endif 
 
-#define THREADS_LIMIT   64
+#define THREADS_LIMIT   8
 #define __NUM_THREADS   (1024 / NUM_ENCLAVE)
 #define NUM_THREADS     (__NUM_THREADS > THREADS_LIMIT ? THREADS_LIMIT : __NUM_THREADS)
 #define MAX_FORK     4
